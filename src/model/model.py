reverted random masking and fixed the range for coords

src/model/model.py
@@ -122,19 +122,20 @@ class Weatherformer(nn.Module):
         batch_size, seq_len, n_features = weather.shape
 
         # temporal index is index in time and temporal granularity ()
-        temporal_granularity = torch.ones(
-            (batch_size,), dtype=torch.int, device=temporal_index.device
-        )  # temporal_index[:, 1].int()
+        temporal_granularity = temporal_index[:, 1].int()
         temp_embedding = self.input_scaler(temporal_granularity)
+
         # mask the masked dimensions and scale the rest
         weather = weather * temp_embedding.view(batch_size, 1, n_features)
-
         if weather_feature_mask is not None:
             # if the shape is batch_size x weather indices, keep the first copy
-            # if len(weather_feature_mask.shape) > 1:
-            #     weather_feature_mask = weather_feature_mask[0, :]
-            # weather[weather_feature_mask] = 0
-            weather = weather * (~weather_feature_mask)
+            if len(weather_feature_mask.shape) > 1:
+                weather_feature_mask = weather_feature_mask[0, :]
+            weather[:, :, weather_feature_mask] = 0
+
+            # scalers for for masked dimensions = true becomes zero
+            # temp_embedding = (~weather_feature_mask).unsqueeze(0) * temp_embedding
+
         weather = self.in_proj(weather)
         # add temporal positional encoding
         # weather += self.temporal_pos_encoding(temporal_granularity).unsqueeze(1)

src/model/train.py
@@ -30,30 +30,6 @@ def swap_features(weather_indices, num_input_features, k=1):
     return weather_indices
 
 
-def create_mlm_mask(batch_size, n_features, n_masked_features, device):
-    # Calculate the total number of elements in the tensor
-    total_elements = batch_size * n_features
-
-    # Calculate the number of elements to mask
-    num_mask = batch_size * n_masked_features
-
-    # Generate random indices for masking within the entire tensor
-    mask_indices = torch.randperm(total_elements)[:num_mask]
-
-    # Convert flat indices to multi-dimensional indices
-    mask_indices = (mask_indices // n_features, mask_indices % n_features)
-
-    # Create the initial mask tensor filled with zeros
-    mask_tensor = torch.zeros(
-        batch_size, n_features, dtype=torch.float32, device=device
-    )
-
-    # Set the randomly selected indices to 1
-    mask_tensor[mask_indices] = True
-
-    return mask_tensor.bool()
-
-
 def train(
     model,
     num_input_features,
@@ -77,37 +53,19 @@ def train(
         optimizer.zero_grad()
 
         # swap some input and output feature of weather indices in place
-        # swap_features(weather_indices, num_input_features, k=num_feature_swaps)
-        # target_indices = weather_indices[num_input_features:]
-        # target_features = data[:, :, target_indices]
-        # target_mask = torch.zeros(TOTAL_WEATHER_VARS, dtype=torch.bool, device=DEVICE)
-        # target_mask[target_indices] = True
-        # target_mask = target_mask.view(1, -1).expand(data.shape[0], -1)
+        swap_features(weather_indices, num_input_features, k=num_feature_swaps)
+        target_indices = weather_indices[num_input_features:]
+        target_features = data[:, :, target_indices]
+        target_mask = torch.zeros(TOTAL_WEATHER_VARS, dtype=torch.bool, device=DEVICE)
+        target_mask[target_indices] = True
+        target_mask = target_mask.view(1, -1).expand(data.shape[0], -1)
 
-        # output = model(data, coords, index, weather_feature_mask=target_mask)[
-        #     :, :, target_indices
-        # ]
-
-        # loss = criterion(target_features, output)
-
-        batch_size, seq_len, n_features = data.size()
-        # true means will be masked
-        weather_feature_mask = create_mlm_mask(
-            batch_size, n_features, num_input_features, device
-        )
-        # expand the mask
-        weather_feature_mask = weather_feature_mask.unsqueeze(1).expand(
-            -1,
-            seq_len,
-            -1,
-        )
-        target_features = data[weather_feature_mask]
-
-        output = model(data, coords, index, weather_feature_mask=weather_feature_mask)[
-            weather_feature_mask
+        output = model(data, coords, index, weather_feature_mask=target_mask)[
+            :, :, target_indices
         ]
 
         loss = criterion(target_features, output)
+
         total_loss += loss.item()
         loader_len += 1
 
@@ -138,35 +96,17 @@ def validate(
     ) in loader:
         data, coords, index = data.to(device), coords.to(device), index.to(device)
         # swap one input and output feature of weather indices in place
-        # swap_features(weather_indices, num_input_features, k=num_feature_swaps)
-        # target_indices = weather_indices[num_input_features:]
-        # target_features = data[:, :, target_indices]
-        # target_mask = torch.zeros(TOTAL_WEATHER_VARS, dtype=torch.bool, device=DEVICE)
-        # target_mask[target_indices] = True
-        # target_mask = target_mask.view(1, -1).expand(data.shape[0], -1)
-
-        # with torch.no_grad():
-        #     output = model(data, coords, index, weather_feature_mask=target_mask)[
-        #         :, :, target_indices
-        #     ]
-
-        batch_size, seq_len, n_features = data.size()
-        # true means will be masked
-        weather_feature_mask = create_mlm_mask(
-            batch_size, n_features, num_input_features, device
-        )
-        # expand the mask
-        weather_feature_mask = weather_feature_mask.unsqueeze(1).expand(
-            -1,
-            seq_len,
-            -1,
-        )
-        target_features = data[weather_feature_mask]
+        swap_features(weather_indices, num_input_features, k=num_feature_swaps)
+        target_indices = weather_indices[num_input_features:]
+        target_features = data[:, :, target_indices]
+        target_mask = torch.zeros(TOTAL_WEATHER_VARS, dtype=torch.bool, device=DEVICE)
+        target_mask[target_indices] = True
+        target_mask = target_mask.view(1, -1).expand(data.shape[0], -1)
 
         with torch.no_grad():
-            output = model(
-                data, coords, index, weather_feature_mask=weather_feature_mask
-            )[weather_feature_mask]
+            output = model(data, coords, index, weather_feature_mask=target_mask)[
+                :, :, target_indices
+            ]
 
         loss = F.mse_loss(target_features, output)
 
@@ -242,9 +182,9 @@ def training_loop(
         losses["val"].append(val_loss)
 
         daily_scaler_mean = model.module.input_scaler.weight[1].mean().item()
-        weekly_scaler_mean = model.module.input_scaler.weight[1].mean().item()
+        weekly_scaler_mean = model.module.input_scaler.weight[7].mean().item()
         logging.info(
-            f"Epoch {epoch+1}: Losses train: {train_loss:.3f} val: {val_loss:.3f}, scaler means: daily {daily_scaler_mean:.3f}"  # , weekly: {weekly_scaler_mean:.3f}"
+            f"Epoch {epoch+1}: Losses train: {train_loss:.3f} val: {val_loss:.3f}, scaler means: daily {daily_scaler_mean:.3f}, weekly: {weekly_scaler_mean:.3f}"
         )
         if epoch % 2 == 1 or epoch == num_epochs - 1:
             torch.save(

src/yield_prediction/bert_model.py
@@ -5,6 +5,7 @@ from .model import TransformerModel
 import torch
 import torch.nn as nn
 import copy
+import math
 
 
 class BERTYieldPredictor(nn.Module):
@@ -119,7 +120,7 @@ class BERTYieldPredictor(nn.Module):
                 practices,
                 year.reshape(batch_size, n_years, 1),
                 y_past.unsqueeze(2),
-                coord.view(batch_size, n_years, 2),
+                coord.view(batch_size, n_years, 2) / 180 * math.pi,
             ),
             dim=2,
         )

src/yield_prediction/model.py
@@ -214,7 +214,7 @@ class YieldPredictor(nn.Module):
                 practices,
                 year.reshape(batch_size, n_years, 1),
                 y_past.unsqueeze(2),
-                coord.view(batch_size, n_years, 2),
+                coord.view(batch_size, n_years, 2) / 180 * math.pi,
             ),
             dim=2,
         )