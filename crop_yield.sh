cec24167b88229087b7c4a5dfdc0e01d0b8eab01
Adib Hasan <adib.hasan8@gmail.com>
2025-05-30 18:20:49 -0400

    fixed crop yield

--- crop_yield.sh
+++ crop_yield.sh
@@ -3,12 +3,11 @@
 #SBATCH -A mit_general
 #SBATCH --job-name=crop_yield_training
 #SBATCH -N 1                            # Single node
-#SBATCH --ntasks=1                      # One task
+#SBATCH --ntasks=1                      # One task (master launcher)
 #SBATCH --cpus-per-task=4              # 4 CPU threads for the task
-#SBATCH --gres=gpu:1             # Request 1 GPU
-#SBATCH --mem=20GB                     # Total memory
-#SBATCH -t 12:00:00                    # 12-hour wall time
-
+#SBATCH --gres=gpu:1                   # Request 4 GPUs
+#SBATCH --mem=40GB                     # Total memory
+#SBATCH -t 24:00:00                    # 24-hour wall time
 # Load your environment
 module load miniforge/24.3.0-0
 source activate torch  # Replace with your conda env
