636ecd5a31b12784b6391467573dc473f41cfcb7
Adib Hasan <adib.hasan8@gmail.com>
2025-06-05 11:46:49 -0400

    dont learn sigma_x

--- src/pretraining/trainers/weatherformer_mixture_trainer.py
+++ src/pretraining/trainers/weatherformer_mixture_trainer.py
@@ -76,6 +76,7 @@ class WeatherFormerMixtureTrainer(WeatherFormerTrainer):
             reconstruction + log_variance, feature_mask, dim=(1, 2)
         )  # mean over masked seq_len, n_features
         enc_loss = enc_loss.mean()  # then mean over batch_size
+        mse_loss = self._masked_mean((z - mu_x) ** 2, feature_mask, dim=(1, 2)).mean()
 
         # ---------- mixture‑prior term ----------
         z = z.unsqueeze(0)  # [1,batch_size,seq_len,n_features]
@@ -91,7 +92,7 @@ class WeatherFormerMixtureTrainer(WeatherFormerTrainer):
         # var_reg = (
         #     0.3 * self._masked_mean(var_x, feature_mask, dim=(1, 2)).mean()
         # )  # penalize large var
-        total = mix_loss + enc_loss  # + var_reg
+        total = mix_loss + enc_loss * 0.000001 + mse_loss  # + var_reg
         if log_losses:
             self.logger.info(f"Encoder Loss: {enc_loss.item():.6f}")
             self.logger.info(f"Mixture Prior Loss: {mix_loss.item():.6f}")
