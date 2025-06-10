fixed cnn masking bug

src/crop_yield/models/weatherautoencoder_mixture_yield_model.py
@@ -84,12 +84,12 @@ class WeatherAutoencoderMixtureYieldModel(WeatherAutoencoderYieldModel):
         var_x = torch.clamp(var_x, min=1e-8, max=1)
         var_k = torch.clamp(var_k, min=1e-8, max=1)
 
-        # Pass through MLP to get yield prediction
+        # we imputed weather, the mask is not necessary
         yield_pred = self.yield_model(
             z,
             coord,
             year,
             interval,
-            weather_feature_mask,
+            weather_feature_mask=None,
         )
         return yield_pred, z, mu_x, var_x, mu_k, var_k

src/crop_yield/models/weatherautoencoder_sine_yield_model.py
@@ -93,13 +93,13 @@ class WeatherAutoencoderSineYieldModel(WeatherAutoencoderYieldModel):
         z = mu_x + torch.sqrt(var_x) * epsilon
         # predict missing only and keep original weather for rest
         z = self._impute_weather(padded_weather, z, weather_feature_mask)
-        # Pass through MLP to get yield prediction
+        # we imputed weather, the mask is not necessary
         yield_pred = self.yield_model(
             z,
             coord,
             year,
             interval,
-            weather_feature_mask,
+            weather_feature_mask=None,
         )
 
         # Clamp variances for numerical stability before returning

src/pretraining/models/weatherbert.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from typing import Optional
+import copy
 
 from src.base_models.vanilla_pos_encoding import VanillaPositionalEncoding
 from src.utils.constants import MAX_CONTEXT_LENGTH, DEVICE
@@ -60,10 +61,10 @@ class WeatherBERT(BaseModel):
                 f"expected max length {self.max_len} but received {pretrained_model.max_len}"
             )
 
-        self.in_proj = pretrained_model.in_proj
-        self.positional_encoding = pretrained_model.positional_encoding
-        self.transformer_encoder = pretrained_model.transformer_encoder
-        self.out_proj = pretrained_model.out_proj
+        self.in_proj = copy.deepcopy(pretrained_model.in_proj)
+        self.positional_encoding = copy.deepcopy(pretrained_model.positional_encoding)
+        self.transformer_encoder = copy.deepcopy(pretrained_model.transformer_encoder)
+        self.out_proj = copy.deepcopy(pretrained_model.out_proj)
 
     def forward(
         self,