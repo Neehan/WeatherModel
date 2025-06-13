d8003bb28735dfef285101804e220aad2f646e8d
Adib Hasan <adib.hasan8@gmail.com>
2025-06-12 21:15:22 -0400

    Revert "removed weather mask from loss and original weather from encoder output"
    This reverts commit 145c9bf680bdf6a17c160faca2bf692ef4a49f17.

--- src/crop_yield/models/weatherbert_yield_model.py
+++ src/crop_yield/models/weatherbert_yield_model.py
@@ -68,12 +68,11 @@ class WeatherBERTYieldModel(BaseModel):
         - imputed_weather: batch_size x seq_len x weather_dim
         - weather_feature_mask: batch_size x seq_len x weather_dim
         """
-        # return (
-        #     original_weather
-        #     * (~weather_feature_mask)  # keep original where mask is False
-        #     + imputed_weather * weather_feature_mask
-        # )
-        return imputed_weather
+        return (
+            original_weather
+            * (~weather_feature_mask)  # keep original where mask is False
+            + imputed_weather * weather_feature_mask
+        )
 
     def load_pretrained(self, pretrained_model: WeatherBERT):
         """
